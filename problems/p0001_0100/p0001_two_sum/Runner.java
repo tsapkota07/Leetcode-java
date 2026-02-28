fd4a19a18f5c72ccd66cefb8e3f59dc88b4279aa
Tirsan Sapkota <tirsanspkt159@gmail.com>
2026-02-27 23:18:23 -0500

    touched base on two sum (AGAIN). Move on to Contains duplicate tomorrow

--- problems/p0001_0100/p0001_two_sum/Runner.java
+++ problems/p0001_0100/p0001_two_sum/Runner.java
@@ -1,20 +1,65 @@
+// Runner class asked to chat gpt.
 package p0001_0100.p0001_two_sum;
+
 import java.util.Arrays;
 
-public class Runner
-{
-    public static void main(String[] args)
-    {
-        p0001_0100.p0001_two_sum.Solution s = new p0001_0100.p0001_two_sum.Solution();
+public class Runner {
 
-        int[] nums1 = {2, 7, 11, 15};
-        int[] nums2 = {2, 0, 1, 4};
-        int target1 = 9;
-        int target2 = 6;
+    private static void runTest(Solution s, int[] nums, int target, String name) {
+        int[] res = s.twoSum(nums, target);
 
-        int[] result1 = s.twoSum(nums1, target1);
-        int[] result2 = s.twoSum(nums2, target2);
-        System.out.println("Test 1: " + Arrays.toString(result1));
-        System.out.println("Test 2: " + Arrays.toString(result2));
+        System.out.println("==== " + name + " ====");
+        System.out.println("nums   = " + Arrays.toString(nums));
+        System.out.println("target = " + target);
+        System.out.println("result = " + Arrays.toString(res));
+
+        if (res == null) {
+            System.out.println("❌ FAIL: returned null\n");
+            return;
+        }
+
+        if (res.length != 2) {
+            System.out.println("❌ FAIL: result length must be 2\n");
+            return;
+        }
+
+        int i = res[0];
+        int j = res[1];
+
+        if (i < 0 || i >= nums.length || j < 0 || j >= nums.length) {
+            System.out.println("❌ FAIL: index out of bounds (i=" + i + ", j=" + j + ")\n");
+            return;
+        }
+
+        if (i == j) {
+            System.out.println("❌ FAIL: used same index twice\n");
+            return;
+        }
+
+        int sum = nums[i] + nums[j];
+        if (sum != target) {
+            System.out.println("❌ FAIL: nums[i] + nums[j] = " + sum + " (expected " + target + ")");
+        } else {
+            System.out.println("✅ PASS: nums[" + i + "] + nums[" + j + "] = "
+                    + nums[i] + " + " + nums[j] + " = " + target);
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        runTest(s, new int[]{2, 7, 11, 15}, 9, "Classic example");
+        runTest(s, new int[]{3, 2, 4}, 6, "Unsorted");
+        runTest(s, new int[]{2, 0, 1, 4}, 6, "Includes zero");
+        runTest(s, new int[]{-3, 4, 3, 90}, 0, "Negatives");
+        runTest(s, new int[]{-10, -20, 5, 7}, -30, "Two negatives");
+        runTest(s, new int[]{1, 5, 9, 13, 20}, 21, "Ends pair");
+
+        // Large but SAFE values (no overflow)
+        runTest(s, new int[]{500_000_000, 400_000_000, 100_000_000},
+                900_000_000,
+                "Large safe values");
     }
 }
--- problems/p0001_0100/p0001_two_sum/Solution.java
+++ problems/p0001_0100/p0001_two_sum/Solution.java
@@ -22,39 +22,64 @@ import java.util.HashMap;
 
 class Solution {
     public int[] twoSum (int[] nums, int target) {
-        HashMap <Integer, Integer> complements = new HashMap <Integer, Integer>();
 
-        for ( int i = 0; i < nums.length; i++) {
-            Integer complementIndex = complements.get(nums[i]);
+        /*
+        Before you understand hashmap, understand what a complement is.
+        if a + b are two numbers who equal the number target.
+        Then, b is the complement of a and a is the complement of b.
+        Also, we can have other complements such as c + d = target. To make it easy on us,
+        We say target's complement of a is b.Also, target's complement of b is a.
+        this means the same as a + b is target.
+         */
 
-            if (complementIndex != null) {
-                return new int[] {i, complementIndex};
+        HashMap <Integer, Integer> complementMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            // Store the index of complement of the current number.
+            Integer indexOfComplement = complementMap.get(nums[i]);
+
+            // if the index is found, return the index of the current number + the index of it's complement.
+            if (indexOfComplement != null) {
+                int[] answer = {i, indexOfComplement};
+                return answer;
             }
-            complements.put (target - nums[i], i);
+
+            // if index is null, meaning the complement of the current number does not yet exist in the map, make sure
+            // to put the complement of this number into the mapping.
+            complementMap.put(target - nums[i], i);
 
         }
-
         return null;
-
     }
 
 
 }
-    /*
-    {5, 2, 4}
-    6
+/*Thinking process:
+HashMap  K -> V
+Key is (target - num} =
+We insert 1 into the map along with its index.
+1 -> 0
+Does the next value, 2,  match the key we have in our hashmap No.{(1,0)} So, we just insert 6-2 = 4 as the Key and 1 as the value.
+4 -> 1
+Does the next value, 4, match the key we have in our hashmap? Yes {(1,0), (4,1)}
+So return the current index, 2 and the index/value we find for the number 4 in our hashmpa,1 . So, our answer is {2,1} .
 
-    HashMap  K -> V
-    Key is (target - num} =
-    We insert 1 into the map along with its index.
-    1 -> 0
-    Does the next value, 2,  match the key we have in our hashmap No.{(1,0)} So, we just insert 6-2 = 4 as the Key and 1 as the value.
-    4 -> 1
-    Does the next value, 4, match the key we have in our hashmap? Yes {(1,0), (4,1)}
-    So return the current index, 2 and the index/value we find for the number 4 in our hashmpa,1 . So, our answer is {2,1} .
-
+One more time, we try solving this using the Hashmap data structure. In each iteration we do the following:
+- get the index of the complement of the current number from our mapping. return null if the complement of current number
+does not yet exist.
+- check if the complementIndex is null or not. If it is null, put the diference between the target and the current number
+in nums along as a key and the current index as the value in our hashmap.
+- if it is not null, just return the current index of the number along with the index of it's complement.
 
 */
+
+//pattern used: Hashmap
+//Time complexity:
+// O(1) best case as the first two numbers might add up to the target.
+// O(n^2) worst case as we might have to deal with extreme hash collisions.
+//      meaning, each get/put () becomes O(n).
+// O (n) average case assuming that hash collisions aren't extreme.
+//Space complexity: O(n)
+//What trick made it work:We traded space for time. the mapping grows with each iteration wrt our array size.
 
 
 
@@ -75,3 +100,19 @@ class Solution {
 //    }
 //
 //}
+
+/*
+Thinking process: take a number and check if it's sum with every other number adds to the target. (n)
+If the current number doesn't work with any other number to get the target, try every other one. (n)
+ */
+
+//pattern used: brute force
+//Time complexity: O(n^2) worst case as two loops are used. O(1) best case as the first two numbers might give us the sum.
+//Space complexity: O(1). We aren't using any extra space after the initial space.
+//What trick made it work: just brute forced it.
+
+
+//pattern used:
+//Time complexity:
+//Space complexity:
+//What trick made it work:
