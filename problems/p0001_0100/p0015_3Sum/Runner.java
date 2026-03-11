9a3b97be278b15463d8c0ddaad3624c3e31ae063
Tirsan Sapkota <tirsanspkt159@gmail.com>
2026-03-11 01:27:04 -0400

    Create LC 15 : 3Sum

---
+++ problems/p0001_0100/p0015_3Sum/Runner.java
@@ -0,0 +1,5 @@
+package p0001_0100.p0015_3Sum;
+
+public class Runner
+{
+}
---
+++ problems/p0001_0100/p0015_3Sum/Solution.java
@@ -0,0 +1,10 @@
+package p0001_0100.p0015_3Sum;
+
+/*
+Given an integer array nums, return all the triplets nums[i], nums[j] and nums[k] such that
+i != j, i != k, j !=k. and nums[i] + nums[j] + nums[k] = 0;
+Notice that the solution must not contain duplicate triplets.
+ */
+public class Solution
+{
+}
