342c813b714c4f8ce0ab34e6dfd5ccfa07843dfc
Tirsan Sapkota <tirsanspkt159@gmail.com>
2026-03-12 00:57:58 -0400

    Add LC 15: 3 Sum

--- problems/p0001_0100/p0015_3Sum/Runner.java
+++ problems/p0001_0100/p0015_3Sum/Runner.java
@@ -1,5 +1,87 @@
 package p0001_0100.p0015_3Sum;
 
-public class Runner
-{
+import java.util.*;
+
+public class Runner {
+
+    private static void runTest(Solution s, int[] nums, List<List<Integer>> expected, String name) {
+        int[] copy = Arrays.copyOf(nums, nums.length);
+        List<List<Integer>> result = s.threeSum(copy);
+
+        System.out.println("==== " + name + " ====");
+        System.out.println("nums     = " + Arrays.toString(nums));
+        System.out.println("expected = " + expected);
+        System.out.println("result   = " + result);
+
+        Set<String> expectedSet = new HashSet<>();
+        for (List<Integer> triplet : expected) {
+            List<Integer> sorted = new ArrayList<>(triplet);
+            Collections.sort(sorted);
+            expectedSet.add(sorted.toString());
+        }
+
+        Set<String> resultSet = new HashSet<>();
+        if (result != null) {
+            for (List<Integer> triplet : result) {
+                List<Integer> sorted = new ArrayList<>(triplet);
+                Collections.sort(sorted);
+                resultSet.add(sorted.toString());
+            }
+        }
+
+        if (expectedSet.equals(resultSet)) {
+            System.out.println("PASS");
+        } else {
+            System.out.println("FAIL");
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        runTest(
+                s,
+                new int[]{-1, 0, 1, 2, -1, -4},
+                Arrays.asList(
+                        Arrays.asList(-1, -1, 2),
+                        Arrays.asList(-1, 0, 1)
+                ),
+                "Basic example"
+        );
+
+        runTest(
+                s,
+                new int[]{0, 1, 1},
+                Collections.emptyList(),
+                "No triplets"
+        );
+
+        runTest(
+                s,
+                new int[]{0, 0, 0},
+                Arrays.asList(
+                        Arrays.asList(0, 0, 0)
+                ),
+                "All zeros"
+        );
+
+        runTest(
+                s,
+                new int[]{-2, 0, 1, 1, 2},
+                Arrays.asList(
+                        Arrays.asList(-2, 0, 2),
+                        Arrays.asList(-2, 1, 1)
+                ),
+                "Multiple triplets"
+        );
+
+        runTest(
+                s,
+                new int[]{1, 2, -2, -1},
+                Collections.emptyList(),
+                "No valid combination"
+        );
+    }
 }
--- problems/p0001_0100/p0015_3Sum/Solution.java
+++ problems/p0001_0100/p0015_3Sum/Solution.java
@@ -1,5 +1,11 @@
 package p0001_0100.p0015_3Sum;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /*
 Given an integer array nums, return all the triplets nums[i], nums[j] and nums[k] such that
 i != j, i != k, j !=k. and nums[i] + nums[j] + nums[k] = 0;
@@ -7,4 +13,47 @@ Notice that the solution must not contain duplicate triplets.
  */
 public class Solution
 {
+    public List<List<Integer>> threeSum(int[] nums) {
+        // first of all, lets start with sorting the list.
+        Arrays.sort(nums);
+//        List<List<Integer>> result = new ArrayList<>();
+        HashSet<List<Integer>> result = new HashSet<>();
+
+        // let's try to use two pointers.
+        for (int i = 0; i < nums.length - 2; i++) // nums.length-1 is already occupied by the last pointer.
+        {
+            int first = i + 1;
+            int last = nums.length - 1;
+
+//            int indexedNum = nums[i];
+
+            while (first < last)
+            {
+//                int firstNum = nums[first];
+//                int lastNum = nums[last];
+
+                if (nums[i] + nums[first] + nums[last] == 0)
+                {
+                    result.add(Arrays.asList(nums[i], nums[first], nums[last]));
+                    first ++;
+                    last --;
+                }
+
+                else if (nums[i] + nums[first] + nums[last] > 0)
+                {
+                    last--;
+                }
+
+//                if (nums[i] + nums[last] + nums[first] < 0)
+                else
+                {
+                    first++;
+                }
+            }
+
+        }
+
+        return result.stream().collect(Collectors.toList());
+    }
+
 }
