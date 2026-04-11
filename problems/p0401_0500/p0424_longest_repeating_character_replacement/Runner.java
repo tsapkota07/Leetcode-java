0a1615916add7af080aa08df20683f81fa394c89
Tirsan Sapkota <tirsanspkt159@gmail.com>
2026-04-10 23:37:57 -0400

    Added placeholder for LC 424. To be solved next time

---
+++ problems/p0401_0500/p0424_longest_repeating_character_replacement/Runner.java
@@ -0,0 +1,6 @@
+package p0401_0500.p0424_longest_repeating_character_replacement;
+
+public class Runner
+{
+
+}
---
+++ problems/p0401_0500/p0424_longest_repeating_character_replacement/Solution.java
@@ -0,0 +1,9 @@
+package p0401_0500.p0424_longest_repeating_character_replacement;
+
+public class Solution
+{
+    public int characterReplacement(String s, int k){
+
+        return 0;
+    }
+}
