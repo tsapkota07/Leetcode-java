92a62815afed8069dcfda2b1b04724beb447df60
Tirsan Sapkota <tirsanspkt159@gmail.com>
2026-04-12 18:01:02 -0400

    Added placeholder for LC 567. To be solved next time

---
+++ problems/p0501_0600/p0567_permutation_in_string/Runner.java
@@ -0,0 +1,5 @@
+package p0501_0600.p0567_permutation_in_string;
+
+public class Runner
+{
+}
---
+++ problems/p0501_0600/p0567_permutation_in_string/Solution.java
@@ -0,0 +1,16 @@
+package p0501_0600.p0567_permutation_in_string;
+/*
+Given two strings, [s1] and [s2], return [true] if [s2] contains a permutation of [s1] or [false] otherwise.
+In other words, return [true] if one of [s1]'s permutation is the substring of [s2].
+
+Constraints:
+    1 <= s1.length, s2.length <= 10^4
+    s1 and s2 consist of lowercase English letters.
+ */
+public class Solution
+{
+    public boolean checkInclusion(String s1, String s2){
+
+        return false;
+    }
+}
